5300b3a65a5ca8624f253816507ed57af5b1a1c9
chchiou <chchiou@users.noreply.github.com>
2014-10-27 14:52:09 +0100

    Update State.java

--- src/template/State.java
+++ src/template/State.java
@@ -9,63 +9,16 @@ import logist.task.TaskDistribution;
 import logist.task.TaskSet;
 import logist.topology.Topology;
 import logist.topology.Topology.City;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class State {
 	
-	private City currentCity;
-	private ArrayList<City> packLocation = new ArrayList<City> ();
-	private TaskSet tasks;
-	
-	public State(ArrayList<City> packLocation) {
-		this.packLocation = packLocation;
-	};
-	
-	
-	/*overload, this is used to create a new initial state*/		
-	public State(City initialCity, TaskSet tasks) {
-
-		currentCity = initialCity;
-
-		/*Initialize the arrayList to store the location*/
-		for (int i=0; i < tasks.size(); i++) {
-			packLocation.add(null);
-		}
-		
-		for (Task task : tasks) {
-			packLocation.add(task.pickupCity.id, task.pickupCity);
-		}
-		this.tasks = tasks;
-	}
-	
-	public State move(City city) {
-		State state = new State(this.packLocation);
-		state.currentCity = city;
-		
-		
-		return State
-	}
-	
-	public State delivery(pdAction action) {
-		
-	}
-	
-	public State pickup(pdAction action) {}
-			
-			
-	}
-	
-	public boolean identical(State s){
-		boolean result = false;
-		int count = 0;
-		for (int i = 0; i < 6; i++){
-			if (this.packLocation.get(i) == s.packLocation.get(i))
-				count ++;
-		}
-		if (count == 6){
-			result = true;
-		}
-		
-		return result;
-	}
+	int capacity;
+	long cost;
+	public City currentCity;
+	public HashSet<Integer>	carriedTasks = new HashSet<Integer>();
+	public HashSet<Integer> deliveredTasks = new HashSet<Integer> ();
+	public Plan plan;
 }
