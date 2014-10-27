bdab7ad6100b5f8fbf8f760e527b28c80d60afc1
Yilin Hu <yilinhu@tsf-476-wpa-4-112.epfl.ch>
2014-10-27 12:06:25 +0100

    update

--- src/template/DeliberativeTemplate.java
+++ src/template/DeliberativeTemplate.java
@@ -11,14 +11,23 @@ import logist.task.TaskSet;
 import logist.topology.Topology;
 import logist.topology.Topology.City;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * An optimal planner for one vehicle.
  */
 @SuppressWarnings("unused")
 public class DeliberativeTemplate implements DeliberativeBehavior {
-
+	
+	
 	enum Algorithm { BFS, ASTAR }
+	
 	
 	/* Environment */
 	Topology topology;
@@ -27,15 +36,18 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 	/* the properties of the agent */
 	Agent agent;
 	int capacity;
-
+	TaskSet carriedTasks;
 	/* the planning class */
 	Algorithm algorithm;
+	HashMap<Integer, HashSet<Task>> taskMap = new HashMap<Integer, HashSet<Task>> ();
 	
 	@Override
 	public void setup(Topology topology, TaskDistribution td, Agent agent) {
+		
 		this.topology = topology;
 		this.td = td;
 		this.agent = agent;
+		this.carriedTasks = null;
 		
 		// initialize the planner
 		int capacity = agent.vehicles().get(0).capacity();
@@ -51,7 +63,6 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 	@Override
 	public Plan plan(Vehicle vehicle, TaskSet tasks) {
 		Plan plan;
-
 		// Compute the plan with the selected algorithm.
 		switch (algorithm) {
 		case ASTAR:
@@ -70,33 +81,118 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 	
 	private Plan naivePlan(Vehicle vehicle, TaskSet tasks) {
 		return null;
-	
 	}
 	
 	private Plan bfsPlan(Vehicle vehicle, TaskSet tasks) {
+		Queue<State> stateQueue = new LinkedList<State> ();
 		City current = vehicle.getCurrentCity();
 		Plan plan = new Plan(current);
-		State goalState = constructGoal(tasks);
-		/* Initialize the state */
-		State state = new State(vehicle, tasks);
-		iterate(vehicle, state);
+		State initalState = initiateState(vehicle, tasks, plan);
+		stateQueue.add(initalState);
+		this.taskMap = null;
+		for(City city : topology.cities()) {
+			taskMap.put(city.id, new HashSet<Task>());
+		}
+		
+		for(Task task : tasks) {
+			taskMap.get(task.pickupCity.id).add(task);
+		}
+		
+		while(!stateQueue.isEmpty()) {
+			State state = stateQueue.remove();
+			for(State nextState: this.nextStates(state)) {
+				stateQueue.add(nextState);
+			}
+		}
+		
+		
 		return plan;
 	}
 	
+	private State initiateState (Vehicle vehicle, TaskSet tasks, Plan plan) {
+		State state = new State();
+		state.plan = plan;
+		state.currentCity = vehicle.getCurrentCity().id;
+		return state;
+	}
+	
+	private int min(int i, int j) {
+		if (i >= j) return j;
+		else return i;
+	}
+	
+	private HashSet<State> nextStates(State state) {
+		
+		HashSet<State> nextStates = new HashSet<State> ();
+		HashSet<Task> currentTasks = taskMap.get(state.currentCity);
+		
+		Iterator<Task> iterator = currentTasks.iterator();
+		while (iterator.hasNext()) {
+			int id = iterator.next().id;
+			if(state.deliveredTasks.contains(id) || state.carriedTasks.contains(id)) 
+				iterator.remove();
+		}
+		
+		if(!state.ifFull && currentTasks.size() > 0) {
+			int available = min(currentTasks.size(), capacity - state.carriedTasks.size());
+			for(Task task : currentTasks) {
+				for(int toTake = 1; toTake <= available; toTake++) {
+					nextStates.addAll(newTake(currentTasks, toTake, state));
+				}
+			}
+		}
+		return null;
+	}
+	
+	HashSet<State> newTake(HashSet<Task> currentTasks, int toTake,
+			State prestate) {
+		HashSet<State> returnStates = new HashSet<State> (); 
+		if( toTake == 1) {
+			for(Task task : currentTasks) {
+				State state = new State();
+				state = prestate;
+				state.carriedTasks.add(task.id);
+				state.plan.appendPickup(task);
+				returnStates.add(state);
+			}
+		} else {
+			for(Task task : currentTasks) {
+				State state = new State();
+				state.carriedTasks.add(task.id);
+				state.plan.appendPickup(task);
+				HashSet<Task> deductTasks = new HashSet<Task> ();
+				deductTasks = currentTasks;
+				deductTasks.remove(task);
+				returnStates.addAll(newTake(deductTasks));
+				
+			}
+			state = new state(
+		}
+		
+		State returnState = new State();
+		
+		returnState = state;
+		for(Task task : currentTList) {
+			if(taked.size() == toTake) {
+				return taked;
+			}
+			
+			if(!taked.contains(task.id) && taked.size() < toTake) {
+				state.add(task.id);
+				toTake --;
+				return newTake(currentTasks, toTake, taked);
+			}
+		}
+		
+		return null;
+	}
 	
 	private void constructGoal(TaskSet tasks) {
 		for (Task task : tasks) {
 		 // to construct the goal state	
 		}
 	}
-	
-	private void iterate(Vehicle vehicle, State state) {
-		
-		
-		if(state.identical(goalState) {
-			return plan;
-		}
-	}
+
 
 	@Override
 	public void planCancelled(TaskSet carriedTasks) {
--- src/template/State.java
+++ src/template/State.java
@@ -12,46 +12,10 @@ import logist.topology.Topology.City;
 import java.util.ArrayList;
 
 public class State {
+	Plan plan;
+	public boolean ifFull = false;
+	public int currentCity;
+	public	ArrayList<Integer>	carriedTasks = new ArrayList<Integer>();
+	public ArrayList<Integer> deliveredTasks = new ArrayList<Integer> ();
 	
-	private Vehicle vehicle;
-	private ArrayList<City> packLocation = new ArrayList<City> ();
-	private TaskSet tasks;
-	
-	public State(Vehicle vehicle, ArrayList<City> packLocation) {
-		this.packLocation = packLocation;
-	};
-	
-	
-	/*overload, this is used to create a new initial state*/		
-	public State(Vehicle vehicle, TaskSet tasks) {
-
-		this.vehicle = vehicle;
-
-		/*Initialize the arrayList to store the location*/
-		for (int i=0; i < tasks.size(); i++) {
-			packLocation.add(null);
-		}
-		
-		for (Task task : tasks) {
-			packLocation.add(task.pickupCity.id, task.pickupCity);
-		}
-		this.tasks = tasks;
-	}
-	
-	public State move(City city) {
-		
-		State state = new State(vehicle, this.packLocation);
-		 = city;
-		
-		return State
-	}
-	
-	public State delivery(pdAction action) {
-		
-	}
-	
-	public State pickup(pdAction action) {}
-			
-			
-	}
 }
